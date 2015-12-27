69707e7300f5cd16a494f5c4775bdf75147fd296
rahmantasnim <t.rahman94@gmail.com>
2015-12-26 22:44:42 -0600

    added more todo list, progress on designing strategy to deal with extracurricular activites

---
+++ data/processing/csv/xtra_curr_processing.py
@@ -0,0 +1,41 @@
+"""
+REDUCED LIST OF EXTRACURRICULARS
+
+01. Student Government
+02. Yearbook
+03. Dance (salsa
+04. Chess
+05. Drama (Theater)
+06. Academic Societies (honor society, arista)
+07. Art (visual Arts, studio art, mural painting)
+08. Robotics
+09. Tutoring (SAT prep, homework help, regents prep)
+10. Photography (Digitial)
+11. Music ??? (Glee)
+12. Cheerleading
+13. Model UN
+14. Peer Mediation (conflict resolution)
+15. Chorus, Choir
+16. Jounalism
+17. Anime
+18. Mock Trial
+19. Step Team
+20. Debate Team
+21. Community Service (Key, red cross)
+22. Math Team
+23. Literary Magazine
+24. Fitness (Yoga, Soccer, Basketball, Swimming)
+25. Book
+26. Film (filmmaking)
+27. Science, science olympiad
+28. Band (Guitar)
+29. Poetry, Creative Writing, Spoken Word
+30. Tech (Computer, computer programming, MOUSE squad)
+31. Gay Straight Alliance
+32. Mentoring
+33. Cooking (Culinary Arts)
+34. Fashion (fashion design
+35. Video Game
+36. Environmental Club
+37. MSA
+"""
